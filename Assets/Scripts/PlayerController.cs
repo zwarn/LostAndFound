47f2391376b1361dc90ae1c66c1e718135c3fd6b
zwarn <michael.budahn@gmail.com>
2021-01-29 22:33:56 +0100

    Basic Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,37 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Input _input;
+    [SerializeField] private float movementSpeed = 5;
+
+    void Awake()
+    {
+        _input = new Input();
+    }
+
+    private void OnEnable()
+    {
+        _input.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _input.Disable();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 movementDirection = _input.Play.Movement.ReadValue<Vector2>();
+        transform.Translate(movementDirection * (movementSpeed * Time.deltaTime));
     }
 }
